1dd47cedccff5dce624d988ca8448795044b3bb2
antoinechalifour <antoine.chalifour@gmail.com>
2019-12-26 18:04:59 +0100

    chore: add abilities

---
+++ dev-server/scripts/v2/migrations/20191226175528_import_abilities.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const { findEntityByLanguageName } = require("../migrations_utils");
+const { getDirectoryContent } = require("../migrations_utils");
+
+const ABILITY_DIR = path.join(__dirname, "../../../data/ability");
+
+const createAbilityTable = async knex =>
+  knex.schema.createTable("ability", table => {
+    table
+      .integer("id")
+      .unsigned()
+      .notNullable()
+      .primary();
+    table.string("name_en").notNullable();
+    table.string("name_fr");
+    table.string("name_ja");
+    table.string("description").notNullable();
+  });
+
+const mapAbilityToAbilityDatabase = ability => ({
+  id: ability.id,
+  name_en: findEntityByLanguageName(ability.names, "en").name,
+  name_fr: findEntityByLanguageName(ability.names, "fr").name,
+  name_ja: findEntityByLanguageName(ability.names, "ja").name,
+  description: findEntityByLanguageName(ability.effect_entries, "en").effect
+});
+
+const insertAllAbilities = async knex => {
+  const allAbilities = await getDirectoryContent(ABILITY_DIR);
+
+  await knex.transaction(async trx => {
+    for (const ability of allAbilities) {
+      const abilityDatabase = mapAbilityToAbilityDatabase(ability);
+
+      await trx.insert(abilityDatabase).into("ability");
+    }
+  });
+};
+
+exports.up = async knex => {
+  await createAbilityTable(knex);
+  await insertAllAbilities(knex);
+};
+
+exports.down = knex => knex.schema.dropTable("ability");
