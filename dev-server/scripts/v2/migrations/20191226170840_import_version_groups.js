8f14eba3bac6449319a05570c94a57c30f249305
antoinechalifour <antoine.chalifour@gmail.com>
2019-12-26 17:23:31 +0100

    chore: add version group

---
+++ dev-server/scripts/v2/migrations/20191226170840_import_version_groups.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const { extractIdFromUrl } = require("../migrations_utils");
+const { findEntityByLanguageName } = require("../migrations_utils");
+const { getDirectoryContent } = require("../migrations_utils");
+
+const VERSION_GROUP_DIR = path.join(__dirname, "../../../data/version-group");
+
+const createVersionGroupTable = async knex =>
+  knex.schema
+    .createTable("version_group", table => {
+      table
+        .integer("id")
+        .unsigned()
+        .notNullable()
+        .primary();
+    })
+    .alterTable("version", table => {
+      table.integer("version_group_id").unsigned();
+      table.foreign("version_group_id").references("version_group.id");
+    });
+
+const mapVersionGroupToVersionGroupDatabase = versionGroup => ({
+  id: versionGroup.id
+});
+
+const insertAllVersionGroups = async knex => {
+  const allVersionGroups = await getDirectoryContent(VERSION_GROUP_DIR);
+
+  await knex.transaction(async trx => {
+    for (const versionGroups of allVersionGroups) {
+      const versionGroupsDatabase = mapVersionGroupToVersionGroupDatabase(
+        versionGroups
+      );
+
+      await trx.insert(versionGroupsDatabase).into("version_group");
+
+      const versionIds = versionGroups.versions
+        .map(x => x.url)
+        .map(url => extractIdFromUrl("version", url));
+
+      for (const versionId of versionIds) {
+        await trx("version")
+          .where({ id: versionId })
+          .update({ version_group_id: versionGroupsDatabase.id });
+      }
+    }
+  });
+};
+
+exports.up = async knex => {
+  await createVersionGroupTable(knex);
+  await insertAllVersionGroups(knex);
+};
+
+exports.down = knex =>
+  knex.schema
+    .alterTable("version", table => {
+      table.dropColumn("version_group_id");
+    })
+    .dropTable("version_group");
