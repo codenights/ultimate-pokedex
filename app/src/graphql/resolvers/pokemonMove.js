18b8e5f10097cd680bf7e7024a261a690f1849f8
antoinechalifour <antoine.chalifour@gmail.com>
2019-12-29 00:49:51 +0100

    perf: implement dataloader for pokemon_move

--- app/src/graphql/resolvers/pokemonMove.js
+++ app/src/graphql/resolvers/pokemonMove.js
@@ -3,5 +3,8 @@ import * as R from "ramda";
 export const PokemonMoveResolver = {
   move: R.identity,
   learn: ({ id, pokemon_id }, args, { pokemonMoveRepository }) =>
-    pokemonMoveRepository.findPokemonMoveByPokemonIdAndMoveId(pokemon_id, id)
+    pokemonMoveRepository.findPokemonMoveByPokemonIdAndMoveId.load({
+      pokemonId: pokemon_id,
+      moveId: id
+    })
 };
