7ac757cfc44f78bf69edf50dca64cb27d13d1851
antoinechalifour <antoine.chalifour@gmail.com>
2019-12-28 22:11:58 +0100

    chore: allow using sqlite or mysql in dev

--- app/knexfile.js
+++ app/knexfile.js
@@ -1,17 +1,31 @@
 require("dotenv").config();
 const path = require("path");
 
-module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./db.sqlite3"
-    },
-    migrations: {
-      directory: path.join(__dirname, "migrations")
-    },
-    useNullAsDefault: true
+let developmentConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./db.sqlite3"
   },
+  migrations: {
+    directory: path.join(__dirname, "migrations")
+  },
+  useNullAsDefault: true
+};
+
+if (process.env.DATABASE_DRIVER === "MYSQL") {
+  developmentConfig = {
+    client: "mysql",
+    connection: {
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME
+    }
+  };
+}
+
+module.exports = {
+  development: developmentConfig,
   production: {
     client: "mysql",
     connection: {
