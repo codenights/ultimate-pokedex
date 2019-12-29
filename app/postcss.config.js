5519f0f38ba04df38c4528727b6f240a8bdf67da
Shipow <shipowlata@gmail.com>
2019-12-29 14:33:37 +0100

    feat: add icons + css colors for types
    # Conflicts:
    #	yarn.lock

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: [
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
     require('tailwindcss'),
     require('autoprefixer')
   ]
