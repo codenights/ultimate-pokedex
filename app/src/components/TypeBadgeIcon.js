11f8a7eb8a0419e6c6db051c5711e04f121de98a
Shipow <shipowlata@gmail.com>
2019-12-30 12:03:15 +0100

    feat: hits design

--- app/src/components/TypeBadgeIcon.js
+++ app/src/components/TypeBadgeIcon.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export const TypeBadgeIcon = ({ type }) => (
   <span className={`inline-block whitespace-no-wrap rounded-full text-center h-8 w-8 bg-type-${type.toLowerCase()}`}>
-    <img className="inline align-text-bottom" src={`./icons/types/${type.toLowerCase()}.svg`} width="12"/>
+    <svg className="fill-current text-gray-900 inline align-text-bottom" width="14" viewBox="0 0 512 512">
+      <use xlinkHref={`./icons/types/${type.toLowerCase()}.svg#${type.toLowerCase()}`}></use>
+    </svg>
   </span>
 );
