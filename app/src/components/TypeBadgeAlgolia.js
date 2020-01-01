6b46198d491184be506a270777a5522ec872b943
Shipow <shipowlata@gmail.com>
2020-01-01 18:44:34 +0100

    feat: facets cosmetics

--- app/src/components/TypeBadgeAlgolia.js
+++ app/src/components/TypeBadgeAlgolia.js
@@ -3,15 +3,26 @@ import PropTypes from "prop-types";
 import { TypeIcon } from "./TypeIcon";
 
 export const TypeBadgeAlgolia = ({ type, count }) => (
-  <span className={`tag w-full flex flex-fill whitespace-no-wrap rounded-full text-center px-2 items-center p-1 my-2`}>
+  <span
+    className={`tag w-full flex flex-fill whitespace-no-wrap rounded-full text-center px-2 items-center p-1 my-2`}
+  >
     <TypeIcon type={type} />
     <span className="type-name text-md ml-2">{type}</span>
-    <span className="text-xs text-gray-600 font-semibold flex-1 text-right pr-1">{count}</span>
+    <span className="text-sm text-gray-900 font-bold flex-1 text-right pr-1">
+      {count}
+    </span>
     <style jsx>{`
       .tag {
         box-shadow: 0 0 3px rgba(0, 0, 0, 0.5), 0 5px 10px rgba(0, 0, 0, 0.5),
-      inset 5px 3px 5px rgba(255, 255, 255, 0.1),
-      inset -1px -10px 10px rgba(54, 59, 70, 0.4);
+          inset 1px 1px 0 rgba(255, 255, 255, 0.2),
+          inset -1px -2px 0 rgba(34, 39, 50, 0.3);
+
+        background-image: linear-gradient(
+          -40deg,
+          rgba(75, 79, 92, 0.7) 35%,
+          rgba(0, 0, 0, 0.4) 90%,
+          var(--color-type-${type.toLowerCase()})
+        );
       }
       .type-name {
         color: var(--color-type-${type.toLowerCase()});
