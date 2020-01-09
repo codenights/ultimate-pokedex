e14bed6732d0d31c628542436753564b59af8dfd
François Chalifour <francois.chalifour@gmail.com>
2020-01-09 20:22:29 +0100

    feat(search): design results list footer

--- app/src/components/Search/PokemonList.js
+++ app/src/components/Search/PokemonList.js
@@ -25,13 +25,65 @@ export const PokemonList = connectInfiniteHits(
           ))}
         </div>
 
-        {hasMore && (
-          <div
-            ref={node => {
-              setObservedNode(node);
-            }}
-          />
-        )}
+        <div className="flex justify-center text-center mb-10">
+          {hasMore ? (
+            <button
+              className="load-more rounded-full text-gray-400 py-2 px-4"
+              onClick={() => refineNext()}
+              ref={node => {
+                setObservedNode(node);
+              }}
+            >
+              Load more Pokemon
+            </button>
+          ) : (
+            pokemons.length > 0 && (
+              <div className="end-of-results text-gray-600">
+                {pokemons.length} Pokemon shown
+              </div>
+            )
+          )}
+        </div>
+
+        <style jsx>{`
+          .load-more {
+            border: 1px solid rgba(255, 255, 255, 0.08);
+            background: linear-gradient(
+              40deg,
+              rgba(255, 255, 255, 0.16),
+              rgba(255, 255, 255, 0.1)
+            );
+            box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.3),
+              inset 1px 1px 1px rgba(255, 255, 255, 0.1);
+          }
+
+          .load-more:hover,
+          .load-more:focus {
+            outline: none;
+            border: 1px solid rgba(255, 255, 255, 0.4);
+            box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.3),
+              inset 1px 1px 1px rgba(255, 255, 255, 0.1),
+              inset 0 0 2px 0 rgba(255, 255, 255, 0.2),
+              inset 0 0 10px rgba(255, 255, 255, 0.2);
+          }
+
+          .end-of-results::after {
+            content: "";
+            display: block;
+            margin: auto;
+            margin-top: 16px;
+            height: 3px;
+            width: 150px;
+            border-radius: 8px;
+            background: linear-gradient(
+              40deg,
+              rgba(255, 255, 255, 0.5),
+              rgba(255, 255, 255, 0.24)
+            );
+            box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.3),
+              inset 1px 1px 1px rgba(255, 255, 255, 0.1);
+          }
+        `}</style>
       </div>
     );
   }
