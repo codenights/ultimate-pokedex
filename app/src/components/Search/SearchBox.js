75b1b5f5a016d606d2fa2793da586bab1d8fffef
François Chalifour <francois.chalifour@gmail.com>
2020-02-23 21:05:16 +0100

    feat(search): design loading indicator

--- app/src/components/Search/SearchBox.js
+++ app/src/components/Search/SearchBox.js
@@ -85,37 +85,35 @@ export const SearchBox = connectSearchBox(props => {
           </svg>
         </button>
 
-        {props.showLoadingIndicator && (
-          <span
-            hidden={props.isSearchStalled === false}
-            className="ais-SearchBox-loadingIndicator"
+        <span
+          hidden={props.isSearchStalled === false}
+          className="ais-SearchBox-loadingIndicator"
+        >
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 38 38"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#fff"
+            className="ais-SearchBox-loadingIcon"
           >
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 38 38"
-              xmlns="http://www.w3.org/2000/svg"
-              stroke="#444"
-              className="ais-SearchBox-loadingIcon"
-            >
-              <g fill="none" fillRule="evenodd">
-                <g transform="translate(1 1)" strokeWidth="2">
-                  <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-                  <path d="M36 18c0-9.94-8.06-18-18-18">
-                    <animateTransform
-                      attributeName="transform"
-                      type="rotate"
-                      from="0 18 18"
-                      to="360 18 18"
-                      dur="1s"
-                      repeatCount="indefinite"
-                    />
-                  </path>
-                </g>
+            <g fill="none" fillRule="evenodd">
+              <g transform="translate(1 1)" strokeWidth="2">
+                <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
+                <path d="M36 18c0-9.94-8.06-18-18-18">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    from="0 18 18"
+                    to="360 18 18"
+                    dur="1s"
+                    repeatCount="indefinite"
+                  />
+                </path>
               </g>
-            </svg>
-          </span>
-        )}
+            </g>
+          </svg>
+        </span>
       </form>
 
       <style global jsx>{`
@@ -182,17 +180,20 @@ export const SearchBox = connectSearchBox(props => {
           height: 14px;
         }
 
-        .ais-SearchBox-reset {
+        .ais-SearchBox-reset,
+        .ais-SearchBox-loadingIndicator {
           position: absolute;
           display: flex;
           justify-content: center;
+          align-items: center;
           width: 40px;
           height: 100%;
           right: 0;
           top: 0;
         }
 
-        .ais-SearchBox-reset[hidden] {
+        .ais-SearchBox-reset[hidden],
+        .ais-SearchBox-loadingIndicator[hidden] {
           display: none;
         }
 
