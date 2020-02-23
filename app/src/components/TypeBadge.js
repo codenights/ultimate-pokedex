3b913a99e7a7d3c351942dbadea590ab64dd08e0
antoinechalifour <antoine.chalifour@gmail.com>
2020-02-23 16:37:16 +0100

    feat: add link to Type badge
    Co-authored-by: François Chalifour <francoischalifour@users.noreply.github.com>

--- app/src/components/TypeBadge.js
+++ app/src/components/TypeBadge.js
@@ -1,25 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {TypeIcon} from "./TypeIcon";
+import Link from "next/link";
+
+import { TypeIcon } from "./TypeIcon";
 
 export const TypeBadge = ({ type }) => (
-<span
-    className={"tag flex whitespace-no-wrap rounded-full text-center px-2 items-center p-1 my-2"}
-  >
-    <TypeIcon type={type.name} />
-    <span className="type-name text-md ml-2">{type.name}</span>
-    
-    <style jsx>{`
-      .type-name {
-        color: var(--color-type-${type.name.toLowerCase()});
-      }
-    `}</style>
-    </span>
+  <Link href="/type/[typeId]" as={`/type/${type.id}`}>
+    <a>
+      <span
+        className={
+          "tag flex whitespace-no-wrap rounded-full text-center px-2 items-center p-1 my-2"
+        }
+      >
+        <TypeIcon type={type.name} />
+        <span className="type-name text-md ml-2">{type.name}</span>
+
+        <style jsx>{`
+          .type-name {
+            // prettier-ignore
+            color: var(--color-type-${type.name.toLowerCase()});
+          }
+        `}</style>
+      </span>
+    </a>
+  </Link>
 );
 
 TypeBadge.propTypes = {
   type: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired
-  })
+    color: PropTypes.string.isRequired,
+  }),
 };
