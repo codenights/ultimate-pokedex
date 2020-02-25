85cfe2be8571721680f8896fe0ec29277a360228
François Chalifour <francois.chalifour@gmail.com>
2020-02-25 21:43:49 +0100

    feat: activate shiny mode on double Shift

--- app/src/components/ShinyMode/ShinyMode.js
+++ app/src/components/ShinyMode/ShinyMode.js
@@ -1,9 +1,9 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 
-const shinyContext = createContext(false);
+const ShinyContext = createContext(false);
 
 export const useShiny = (spriteUrl, spriteShinyUrl) => {
-  const isShiny = useContext(shinyContext);
+  const isShiny = useContext(ShinyContext);
 
   return isShiny ? spriteShinyUrl : spriteUrl;
 };
@@ -12,14 +12,44 @@ export const ShinyMode = ({ children }) => {
   const [isShiny, setShiny] = useState(false);
 
   useEffect(() => {
+    let isWaitingForSecondKeyDown = false;
+    let lastKeyDownTimerId = null;
+
+    // This turns the Pokédex in shiny mode on double press on the Shift key.
     function onKeyDown(event) {
+      const element = event.target;
+      const tagName = element.tagName;
+
+      // We don't trigger the shiny mode when typing in text fields
       if (
-        event.key === "Alt" &&
+        element.isContentEditable ||
+        tagName === "INPUT" ||
+        tagName === "SELECT" ||
+        tagName === "TEXTAREA"
+      ) {
+        return;
+      }
+
+      if (
+        event.key === "Shift" &&
+        event.altKey === false &&
         event.ctrlKey === false &&
-        event.shiftKey === false &&
         event.metaKey === false
       ) {
-        setShiny(prevValue => !prevValue);
+        clearTimeout(lastKeyDownTimerId);
+
+        if (isWaitingForSecondKeyDown) {
+          setShiny(prevValue => !prevValue);
+          isWaitingForSecondKeyDown = false;
+        } else {
+          isWaitingForSecondKeyDown = true;
+          lastKeyDownTimerId = setTimeout(() => {
+            isWaitingForSecondKeyDown = false;
+          }, 1000);
+        }
+      } else {
+        // If another key is pressed, we reset the counter
+        isWaitingForSecondKeyDown = false;
       }
     }
 
@@ -31,6 +61,6 @@ export const ShinyMode = ({ children }) => {
   }, []);
 
   return (
-    <shinyContext.Provider value={isShiny}>{children}</shinyContext.Provider>
+    <ShinyContext.Provider value={isShiny}>{children}</ShinyContext.Provider>
   );
 };
