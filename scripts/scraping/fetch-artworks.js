7fabbb34d2912947989a11b8e4fc0c2774991966
François Chalifour <francois.chalifour@gmail.com>
2020-02-25 19:44:30 +0100

    feat: add 8th generation artworks

--- scripts/scraping/fetch-artworks.js
+++ scripts/scraping/fetch-artworks.js
@@ -3,16 +3,16 @@ const { writeFile } = require("fs-extra");
 const axios = require("axios");
 
 const DIR = path.join(__dirname, "../../app/public/artwork");
-const POKEMON_COUNT = 809;
+const POKEMON_COUNT = 890;
 
 for (let nationalId = 1; nationalId <= POKEMON_COUNT; nationalId += 1) {
-  const assetUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${nationalId
+  const assetUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${nationalId
     .toString()
     .padStart(3, "0")}.png`;
 
   axios
     .get(assetUrl, {
-      responseType: "arraybuffer"
+      responseType: "arraybuffer",
     })
     .then(response => {
       writeFile(path.join(DIR, `${nationalId}.png`), response.data);
