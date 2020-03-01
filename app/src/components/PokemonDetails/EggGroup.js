77a2f32da81b9432dad244c0749374fdf41f9c83
François Chalifour <francois.chalifour@gmail.com>
2020-03-01 18:46:05 +0100

    fix: linkify egg group icon

--- app/src/components/PokemonDetails/EggGroup.js
+++ app/src/components/PokemonDetails/EggGroup.js
@@ -3,23 +3,22 @@ import styled from "styled-components";
 import Link from "next/link";
 import { Icons } from "../Icons";
 
-const EggIcon = styled.dd`{
+const EggIcon = styled.dd``;
 
-}`;
-
-
-export const EggGroup = ({id,name}) => {
+export const EggGroup = ({ id, name }) => {
   return (
-      <EggIcon className="inline mr-2">
-        <Icons
-          icon="eggColor"
-          className={`inline mr-1 fill-group-${name.toLowerCase()}`}
-          width="18"
-          height="18"
-        />
-        <Link href="/egg-group/[eggGroupId]" as={`/egg-group/${id}`}>
-          <a>{name}</a>
-        </Link>
-      </EggIcon>
+    <EggIcon className="inline mr-2">
+      <Link href="/egg-group/[eggGroupId]" as={`/egg-group/${id}`}>
+        <a>
+          <Icons
+            icon="eggColor"
+            className={`inline mr-1 fill-group-${name.toLowerCase()}`}
+            width="18"
+            height="18"
+          />
+          {name}
+        </a>
+      </Link>
+    </EggIcon>
   );
 };
