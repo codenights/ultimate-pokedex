a654d9ee6569da14c3a457097ff1b8a54e72fe90
François Chalifour <francois.chalifour@gmail.com>
2020-03-10 18:31:21 +0100

    fix(details): align egg groups

--- app/src/components/PokemonDetails/EggGroup.js
+++ app/src/components/PokemonDetails/EggGroup.js
@@ -1,24 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import Link from "next/link";
 import { Icons } from "../Icons";
 
-const EggIcon = styled.dd``;
-
 export const EggGroup = ({ id, name }) => {
   return (
-    <EggIcon className="inline mr-2">
-      <Link href="/egg-group/[eggGroupId]" as={`/egg-group/${id}`}>
-        <a>
-          <Icons
-            icon="eggColor"
-            className={`inline mr-1 fill-group-${name.toLowerCase()}`}
-            width="18"
-            height="18"
-          />
-          {name}
-        </a>
-      </Link>
-    </EggIcon>
+    <Link href="/egg-group/[eggGroupId]" as={`/egg-group/${id}`}>
+      <a className="flex items-center">
+        <Icons
+          icon="eggColor"
+          className={`inline mr-1 fill-group-${name.toLowerCase()}`}
+          width="18"
+          height="18"
+        />
+        {name}
+      </a>
+    </Link>
   );
 };
